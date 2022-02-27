408145f001bc43f2ed2a12a49b3d309b86086b08
shimabox <shimabox@gmail.com>
2022-02-28 08:28:09 +0900

    :sparkles: append.go
    - append への最初のパラメータ s は、追加元となる T 型のスライス
    - 残りの vs は、追加する T 型の変数群
    - もし、元の配列 s が、変数群を追加する際に容量が小さい場合は、より大きいサイズの配列を割り当て直す
    - その場合、戻り値となるスライスは、新しい割当先を示すようになる

---
+++ src/moretypes/moretypes15/append.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s []int
+	printSlice(s)
+
+	// append works on nil slices.
+	s = append(s, 0)
+	printSlice(s)
+
+	// The slice grows as needed.
+	s = append(s, 1)
+	printSlice(s)
+
+	// s は len=2 cap=2 [0 1] の状態
+	// 3つ要素を加えるので容量が足りなくなり、capが1つ増える
+	s = append(s, 2, 3, 4)
+	printSlice(s)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
