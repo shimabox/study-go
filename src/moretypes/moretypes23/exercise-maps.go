21139118ad4e593e0969c377cf55859941608519
shimabox <shimabox@gmail.com>
2022-02-28 09:10:13 +0900

    :cop: exercise-maps.go
    - TODO
    - golang.org/x/tour/pic のimportが必要

---
+++ src/moretypes/moretypes23/exercise-maps.go
@@ -0,0 +1,15 @@
+package main
+
+// import (
+// 	"golang.org/x/tour/wc"
+// )
+
+// TODO
+func WordCount(s string) map[string]int {
+	return map[string]int{"x": 1}
+}
+
+// TODO
+func main() {
+	// wc.Test(WordCount)
+}
