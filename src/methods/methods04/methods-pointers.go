dc4a5d47251725bb9632975eec52677ab8c69258
shimabox <shimabox@gmail.com>
2022-03-01 19:47:03 +0900

    :sparkles: methods-pointers.go
    - ポインタレシーバでメソッドを宣言できる
    - レシーバ自身を更新できる

---
+++ src/methods/methods04/methods-pointers.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func main() {
+	v := Vertex{3, 4}
+	v.Scale(10)
+	// (v *Vertex) Scale であれば
+	// v == {30 40}
+	// (v Vertex) Scale であれば
+	// v == {3 4} のまま
+	fmt.Println(v.Abs())
+}
