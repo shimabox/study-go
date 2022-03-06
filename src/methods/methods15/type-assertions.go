9d2b536c74aa9bbff8237c017bb50632fe4c7687
shimabox <shimabox@gmail.com>
2022-03-06 11:06:16 +0900

    :sparkles: type-assertions.go
    - 型アサーション は、インターフェースの値の基になる具体的な値を利用する手段を提供する
    - `t := i.(T)`
      - この文は、インターフェースの値 i が具体的な型 T を保持し、基になる T の値を変数 t に代入することを主張する
      - i が T を保持していない場合、この文は panic を引き起こす
    - インターフェースの値が特定の型を保持しているかどうかを テスト するために、型アサーションは2つの値(基になる値とアサーションが成功したかどうかを報告するブール値)を返すことができる
      - `t, ok := i.(T)`
      - i が T を保持していれば、 t は基になる値になり、 ok は真(true)になる
      - そうでなければ、 ok は偽(false)になり、 t は型 T のゼロ値になる => panicは起きない

---
+++ src/methods/methods15/type-assertions.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i interface{} = "hello"
+
+	s := i.(string)
+	fmt.Println(s)
+
+	s, ok := i.(string)
+	fmt.Println(s, ok)
+
+	f, ok := i.(float64)
+	fmt.Println(f, ok)
+
+	// panic: interface conversion: interface {} is string, not float64
+	// f = i.(float64)
+	// fmt.Println(f)
+}
